c36dec05ff8dbd99278593dc88bc8e1f42bb4130
crash2bash <sachagurov@gmail.com>
2020-04-22 10:18:42 +0300

    Updates

---
+++ precache-manifest.32fdd3bc19d3f43fd8c68282ea9b3de3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "917c1b44c731e6c268c1657c412e053a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a97a2d55a0eb81388b18",
+    "url": "./static/css/main.3575e3cb.chunk.css"
+  },
+  {
+    "revision": "b23cd99607aafc7c9c87",
+    "url": "./static/js/2.bc76db86.chunk.js"
+  },
+  {
+    "revision": "85b12af60ddf57ea6860c3b16407fd52",
+    "url": "./static/js/2.bc76db86.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a97a2d55a0eb81388b18",
+    "url": "./static/js/main.bdee9c68.chunk.js"
+  },
+  {
+    "revision": "d34856cc17b978b4a115",
+    "url": "./static/js/runtime-main.9d7a78c6.js"
+  }
+]);
